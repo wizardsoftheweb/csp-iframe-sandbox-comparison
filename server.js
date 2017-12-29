826e26217921d870961d794c64997d5bcb388870
CJ Harries <cj@wizardsoftheweb.pro>
2017-12-29 15:12:03 -0600

    Add sandbox properties

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const SANDBOX_VALUES = [
+    // "forms",
+    // "same-origin",
+    "scripts",
+    // "popups,",
+    "modals,",
+    // "orientation-lock,",
+    // "pointer-lock,",
+    // "presentation,",
+    // "popups-to-escape-sandbox,",
+    // "top-navigation",
+];
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
